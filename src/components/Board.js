fba8102cedc17f3d68f82b1080f0d4111be2d67d
Frank Yu <frankyu1999@gmail.com>
2020-06-07 01:25:23 -0400

    Formatting changes, other changes

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {fabric} from 'fabric';
+import { fabric } from 'fabric';
 import update from 'immutability-helper';
+import { STARTING_BOARD } from '../utils/constants';
 
 //May not need this
 const pieceToImgMap = {
@@ -12,130 +13,164 @@ const pieceToImgMap = {
   queen: 'q',
 };
 
-const turnMap = {0: 'white', 1: 'black'};
-const colorToTurnMap = {l: 'white', d: 'black'};
+const turnMap = { 0: 'white', 1: 'black' };
+const colorToTurnMap = { l: 'white', d: 'black' };
 
 class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      board: this.getBoardPieces(),
+      board: STARTING_BOARD, //TODO: persist the board into localStorage
       turn: 'black',
       turnCount: -1,
-      pieces: {white: [], black: []},
+      pieces: {
+        white: [],
+        black: []
+      },
+      moves: [], //valid moves
       highlights: [] //Make highlight deletion more efficient
     };
   }
 
   //Converts discrete square locations to actual x, y coordinates. Ex. (1,1) -> (80, 80)
   //Gives the center coordinate
-  squareToCoord(x, y) {
-    return {x: x*this.props.size/8 + this.props.size/16, y: y*this.props.size/8 + this.props.size/16};
+  squareToCoord = (x, y) => {
+    const { size } = this.props
+    return { 
+      x: x * size / 8 + size / 16, 
+      y: y * size / 8 + size / 16
+    };
   }
   //Reverse of squareToCoord
-  coordToSquare(x, y) {
-    let division = this.props.size/8;
-    return {x: Math.floor(x/division), y: Math.floor(y/division)};
+  coordToSquare = (x, y) => {
+    let division = this.props.size / 8;
+    return {
+      x: Math.floor(x / division),
+      y: Math.floor(y / division)
+    };
   }
 
   componentDidMount() {
-    //Create the Piece class
-    // var Piece = fabric.util.createClass(fabric.Image, {
-    //   type: 'piece',
-
-    //   initialize: function(options) {
-    //     options || (options = {});
-
-    //     this.callSuper('initialize', options);
-    //     this.set('pieceType', options.pieceType)
-    //   },
-    //   toObject: function() { //Serialization function?
-    //     return fabric.util.object.extend(this.callSuper('toObject'), {
-    //       pieceType: this.get('pieceType')
-    //     });
-    //   },
-    //   //Render function
-    //   _render: function(ctx) {
-
-    //   }
-    // });
-
-    //Instantiate the fabricJS canvas
-    var canvas = new fabric.Canvas('boardCanvas', {
+    //Instantiate the Fabric.js canvas
+    const canvas = new fabric.Canvas('boardCanvas', {
       width: this.props.size,
       height: this.props.size,
       selection: false
     });
+    this.canvas = canvas;
 
     this.drawBackground(canvas);
     this.drawPieces(canvas);
 
-    var moves = [];
-
-    canvas.on('mouse:down', e => {
-      console.log('mouse:down');
-
-      if(e.target && e.target.piece && e.transform) { //If an object/piece was clicked
-        let squareCoords = this.coordToSquare(e.pointer.x - e.transform.offsetX, e.pointer.y - e.transform.offsetY);
-        moves = this.getValidMoves(squareCoords, e.target.pieceChars);
-        console.log(moves);
-
-        this.highlightSquares(canvas, moves);
-      }
-    });
-
+    //Show valid moves on click for pieces
+    canvas.on('mouse:down', this.handleMouseDown);
     //Moving pieces
-    canvas.on('mouse:up:before', e => {
-      console.log(e);
-      if (e.target && e.target.piece && e.transform) { //If an object was clicked
-        //Get the square coords of destination
-        let squareCoords = this.coordToSquare(e.pointer.x - e.transform.offsetX, e.pointer.y - e.transform.offsetY);
-        console.log(squareCoords);
-        //Valid move
-        if(moves.some(e => e.x === squareCoords.x && e.y === squareCoords.y)) {
-          //Relay changes to the model (this.state.board)
-          console.log('valid');
-          //Square coords of the origin
-          let fromCoord = this.coordToSquare(e.transform.original.left, e.transform.original.top);
+    canvas.on('mouse:up:before', this.handleBeforeMouseUp);
+    canvas.on('mouse:up', () => {
+      console.log('after mouseup');
+      //this.forceUpdate();
+      // console.log(this.state);
+      // const whitePieces = this.state.pieces.white
+      // //console.log(whitePieces)
+      // const selMap = whitePieces.map(elem => {
+      //   //console.log(elem)
+      //   return elem.selectable;
+      // });
 
-          let newBoard = this.clone2DArray(this.state.board);
-          newBoard[fromCoord.y][fromCoord.x] = 'e'; //Old spot is empty
-          newBoard[squareCoords.y][squareCoords.x] = e.target.pieceChars; //New spot is occupied
-
-          this.setState({board: newBoard}); //Update the board state...
-          
-          //Center the piece relative to the square
-          let corrected = this.squareToCoord(squareCoords.x, squareCoords.y);
-          e.target.set({
-            left: corrected.x,
-            top: corrected.y
-          });
-          e.target.setCoords();
-
-          //Next player's turn
-          this.nextTurn();
-
-        } else { //Invalid move
-          console.log('invalid move!');
-          //Move the object back
-          e.target.set({
-            left: e.transform.original.left,
-            top: e.transform.original.top
-          });
-          e.target.setCoords();
-        }
-      }
-      //Remove all highlighting
-      this.removeHighlights(canvas);
-    });
+      // console.log(selMap);
+      // console.log(this.canvas);
+    })
   }
-  //Proceed to the next player's turn
-  nextTurn() {
-    //onsole.log(this.state.pieces);
-    //console.log(this.state.pieces[this.state.turn].length);
 
-    //Can't move current player's pieces
+  handleMouseDown = e => {
+    //console.log('mouse:down');
+
+    if(e.target?.piece && e.transform) { //If an object/piece was clicked
+      let squareCoords = this.coordToSquare(e.pointer.x - e.transform.offsetX, e.pointer.y - e.transform.offsetY);
+
+      this.setState({ moves: this.getValidMoves(squareCoords, e.target.pieceChars) }, () => {
+        const { moves } = this.state;
+        //console.log(moves);
+        this.highlightSquares(this.canvas, moves);
+      });
+    }
+  }
+
+  handleBeforeMouseUp = e => {
+    console.log(e);
+
+    /*********************************************
+     * DEBUG START
+     *********************************************/
+
+    // const selMap = this.state.pieces?.white?.map(elem => {
+    //   //console.log(elem)
+    //   return elem.selectable;
+    // });
+
+    // console.log(selMap);
+    let whitePawns = this.canvas.getObjects('piece').filter(elem => elem.pieceChars === 'pl');
+    console.log(whitePawns.map(elem => elem.selectable));
+
+    /*********************************************
+     * DEBUG END
+     *********************************************/
+
+    if (e.target?.piece && e.transform) { //If an object was clicked
+      //TODO: Fix this. Not the piece's turn
+      if (this.state.turn !== colorToTurnMap[e.target.pieceChars[1]]) {
+        throw new Error("It's not your turn!");
+      }
+      //Get the square coords of destination
+      let squareCoords = this.coordToSquare(e.pointer.x - e.transform.offsetX, e.pointer.y - e.transform.offsetY);
+      //console.log(squareCoords);
+
+      //Valid move
+      if (this.state.moves.some(e => e.x === squareCoords.x && e.y === squareCoords.y)) {
+        //Relay changes to the model (this.state.board)
+        //console.log('valid');
+        //Square coords of the origin
+        let fromCoord = this.coordToSquare(e.transform.original.left, e.transform.original.top);
+
+        let newBoard = this.clone2DArray(this.state.board);
+        newBoard[fromCoord.y][fromCoord.x] = 'e'; //Old spot is empty
+        newBoard[squareCoords.y][squareCoords.x] = e.target.pieceChars; //New spot is occupied
+
+        this.setState({board: newBoard}); //Update the board state...
+        
+        //Center the piece relative to the square
+        let corrected = this.squareToCoord(squareCoords.x, squareCoords.y);
+        e.target.setOptions({
+          left: corrected.x,
+          top: corrected.y,
+          selectable: false,
+        });
+        //console.log(e.target);
+        e.target.setCoords();
+        e.target.selectable = false;
+
+        //Next player's turn
+        this.nextTurn();
+
+      } else { //Invalid move
+        console.log('invalid move!');
+        //Move the object back
+        e.target.set({
+          left: e.transform.original.left,
+          top: e.transform.original.top
+        });
+        e.target.setCoords();
+      }
+    }
+    //Remove all highlighting
+    this.removeHighlights(this.canvas);
+  }
+
+  //Proceed to the next player's turn
+  nextTurn = () => {
+    //Can't move current player's pieces when it's the next turn
     this.state.pieces[this.state.turn].forEach(piece => {
+      //console.log(piece);
       console.log('set unselectable');
       piece.set({selectable: false});
     });
@@ -145,19 +180,22 @@ class Board extends React.Component {
       this.setState({turn: turnMap[(this.state.turnCount + 2) % 2]}, () => {
         //Next player's turn
         this.state.pieces[this.state.turn].forEach(piece => {
-          console.log('set selectable');
+          //console.log('set selectable');
           piece.set({selectable: true});
         });
       }));
+
+    //this.setState({ pieces: { ...this.state.pieces }})
   }
-  //Clone a 2D array
-  clone2DArray(array) {
+
+  clone2DArray = array => {
     let clone = [];
     array.forEach(row => {
       clone.push(row.slice(0));
     });
     return clone;
   }
+
   addIfValid(array, x, y) {
     //TODO: check for blocking and Check?
     if(x >= 0 && x <= 7 
@@ -168,7 +206,7 @@ class Board extends React.Component {
   }
 
   //Helper method for getValidMoves()
-  addMoves(array, coords, deltaX, deltaY) {
+  addMoves = (array, coords, deltaX, deltaY) => {
     let tempCoords = {};
     Object.assign(tempCoords, coords); //Clone object
 
@@ -192,21 +230,21 @@ class Board extends React.Component {
     }
   }
   //Helper method for getValidMoves()
-  addBishopMoves(array, coords) {
+  addBishopMoves = (array, coords) => {
     this.addMoves(array, coords, 1, 1);
     this.addMoves(array, coords, 1, -1);
     this.addMoves(array, coords, -1, 1);
     this.addMoves(array, coords, -1, -1);
   }
   //Helper method for getValidMoves()
-  addRookMoves(array, coords) {
+  addRookMoves = (array, coords) => {
     this.addMoves(array, coords, 1, 0);
     this.addMoves(array, coords, -1, 0);
     this.addMoves(array, coords, 0, 1);
     this.addMoves(array, coords, 0, -1);
   }
   //Get a list of valid moves for each piece type...
-  getValidMoves(coords, pieceChars) {
+  getValidMoves = (coords, pieceChars) => {
     let validMoves = [];
     if(pieceChars[0] === 'p') { //Pawn
       if(pieceChars[1] === 'd') { //Black pawn
@@ -245,61 +283,65 @@ class Board extends React.Component {
       this.addIfValid(validMoves, coords.x - 1, coords.y + 1);
     } else {
       console.error('Error: Not a piece!');
+      throw new Error('Error: Not a valid piece!');
     }
 
     return validMoves;
   }
-  //Hightlight a list of squares
-  highlightSquares(canvas, squareList) {
-    let highlights = [];
+
+  /**
+   * Hightlight a list of squares on the board
+   * @param {Object} canvas - Fabric.js canvas to draw on
+   * @param {Array} squareList - A list of coordinates to highlight
+   */
+  highlightSquares = (canvas, squareList) => {
+    const { size } = this.props;
+    const highlights = [];
+
     squareList.forEach(coord => {
-      let square = new fabric.Rect({
+      const square = new fabric.Rect({
         opacity: 0.3,
         fill: "yellow",
-        height: this.props.size/8, 
-        width: this.props.size/8,
-        left: coord.x*this.props.size/8,
-        top: coord.y*this.props.size/8,
+        height: size / 8, 
+        width:  size / 8,
+        left: coord.x * size / 8,
+        top: coord.y * size / 8,
         selectable: false
       });
       highlights.push(square);
       canvas.add(square);
     });
 
-    this.setState({highlights: highlights});
+    this.setState({ highlights });
   }
+
   //Remove all highlights
-  removeHighlights(canvas) {
+  removeHighlights = canvas => {
     this.state.highlights.forEach(square => {
       canvas.remove(square);
     });
     this.setState({highlights: []});
   }
-  //Starting board arrangment...
-  getBoardPieces() {
-    return [['rd','nd','bd','qd','kd','bd','nd','rd'],
-      ['pd','pd','pd','pd','pd','pd','pd','pd'],
-      ['e','e','e','e','e','e','e','e'],
-      ['e','e','e','e','e','e','e','e'],
-      ['e','e','e','e','e','e','e','e'],
-      ['e','e','e','e','e','e','e','e'],
-      ['pl','pl','pl','pl','pl','pl','pl','pl'],
-      ['rl','nl','bl','ql','kl','bl','nl','rl']];
-  }
-  drawBackground(canvas) {
+
+  /**
+   * Draws the board's checker pattern on the Fabric.js canvas
+   * @param {Object} canvas - The Fabric.js Canvas to draw the board squares on.
+   */
+  drawBackground = canvas => {
+    const { size } = this.props
     //Draw the chessboard background
-    for(let y = 0; y < 8; y++) {
-      for(let x = 0; x < 8; x++) {
-        if((x + y) % 2 === 0) { //Omit some squares to create checkerboard pattern
+    for (let y = 0; y < 8; y++) {
+      for (let x = 0; x < 8; x++) {
+        if ((x + y) % 2 === 0) { //Omit some squares to create checkerboard pattern
           continue;
         }
         //Create the square object
         let square = new fabric.Rect({
           fill: "darkcyan",
-          height: this.props.size/8, 
-          width: this.props.size/8,
-          left: x*this.props.size/8,
-          top: y*this.props.size/8,
+          height: size / 8, 
+          width: size / 8,
+          left: x * size / 8,
+          top: y * size / 8,
           selectable: false,
           hoverCursor: 'default'
         });
@@ -307,8 +349,9 @@ class Board extends React.Component {
       }
     }
   }
+
   //Return Promise that returns a fabric.Image. 
-  getPiecePromise(pieceChars, coords) {
+  getPiecePromise = (pieceChars, coords) => {
     let actualCoords = this.squareToCoord(coords.x, coords.y);
     return new Promise((resolve, reject) => {
       fabric.Image.fromURL(require(`../assets/icons/Chess_${pieceChars}t60.png`), oImg => {
@@ -324,33 +367,34 @@ class Board extends React.Component {
         left: actualCoords.x,
         top: actualCoords.y,
         hasBorders: false,
-        piece: true,
         pieceChars: pieceChars,
         squareX: coords.x,
-        squareY: coords.y
+        squareY: coords.y,
+        type: 'piece',
+        piece: true
       });
     });
   }
   //Draw the pieces on the board as represented by the "board" object
   //Use for instantiation only? Then use deltas to account for any changes...
-  drawPieces(canvas) {
+  drawPieces = canvas => {
     //Render the pieces on the board
     let whitePieces = [];
     let blackPieces = [];
 
     let piecePromises = [];
 
+    //Build the promises array
     for(let y = 0; y < 8; y++) {
       for(let x = 0; x < 8; x++) {
         //If it's not empty put a piece there
         if(this.state.board[y][x] !== 'e') {
-          let coords = this.squareToCoord(x, y);
-
           piecePromises.push(this.getPiecePromise(this.state.board[y][x], {x, y}));
         }
       }
     }
 
+    //Wait for all the Promises to return
     Promise.all(piecePromises).then(values => {
       //Put all the pieces on the board
       values.forEach(img => {
@@ -365,11 +409,16 @@ class Board extends React.Component {
       });
 
       //Make it the next turn
-      this.setState({pieces: {white: whitePieces, black: blackPieces}}, () =>
-        this.nextTurn());
+      this.setState(
+        {
+          pieces: { white: whitePieces, black: blackPieces }
+        }, 
+        () => this.nextTurn()
+      );
     });
 
   }
+
   render() {
     return <div style={{display: "flex", "flexFlow": "column", "alignItems": "center"}}>
       <div style={{display: "flex", width: `${this.props.size}px`}}>
---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+
+export const STARTING_BOARD =  [['rd','nd','bd','qd','kd','bd','nd','rd'],
+                              ['pd','pd','pd','pd','pd','pd','pd','pd'],
+                              ['e','e','e','e','e','e','e','e'],
+                              ['e','e','e','e','e','e','e','e'],
+                              ['e','e','e','e','e','e','e','e'],
+                              ['e','e','e','e','e','e','e','e'],
+                              ['pl','pl','pl','pl','pl','pl','pl','pl'],
+                              ['rl','nl','bl','ql','kl','bl','nl','rl']];
