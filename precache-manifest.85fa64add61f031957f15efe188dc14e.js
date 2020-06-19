6ab00e72a45d989a24f61e3c4c06d3f8d5a0751f
Frank <frankyu1999@gmail.com>
2020-06-18 23:29:53 -0400

    Updates

---
+++ precache-manifest.85fa64add61f031957f15efe188dc14e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42ac0e7c8d2fd3cbc93f30e25d0c0f0f",
+    "url": "/Chess-App/index.html"
+  },
+  {
+    "revision": "9e9505e2a3dd84f9f182",
+    "url": "/Chess-App/static/css/main.8aaeed4b.chunk.css"
+  },
+  {
+    "revision": "385271fc3257641e3381",
+    "url": "/Chess-App/static/js/2.707143f7.chunk.js"
+  },
+  {
+    "revision": "74549c08325e899dd17d787a1ff374cf",
+    "url": "/Chess-App/static/js/2.707143f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e9505e2a3dd84f9f182",
+    "url": "/Chess-App/static/js/main.4c6be051.chunk.js"
+  },
+  {
+    "revision": "cf44050fc2e3cecd84d0",
+    "url": "/Chess-App/static/js/runtime-main.7a0d26e4.js"
+  }
+]);
